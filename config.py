03b5bce770db2cf9a786d572d68d976b51b5dcd4
somu974323 <somu974323@gmail.com>
2025-07-25 06:08:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002778873953"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "False").lower() == "true"  # Set "True" For accept new requests
 
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://vinod974323:hYGsyibIw10Fp7x1@cluster0.6pbo1a2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = os.environ.get("DB_NAME", "SKMOVIESZ")
+DB_NAME = os.environ.get("DB_NAME", "Goodmorning")
 
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNEL", "-1002778873953").split())) # Add Multiple channel ids
